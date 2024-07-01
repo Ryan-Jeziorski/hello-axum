70f8e23cc536e861ebccb1071e7ebbff57f8c0ce
Ryan <rjeziorski@outlook.com>
2024-06-30 21:27:15 -0500

    Add multiple pages, modified routing code.

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,12 @@ use tower_http::services::ServeDir;
 async fn main() {
     // Set static html directory to server from
     // build our application with a route
-    let app = Router::new().route_service(
-        "/", 
-        ServeDir::new("static"));
+    let app = Router::new()
+        .route_service("/", ServeDir::new("static/homepage"))
+        .nest_service("/hompage", ServeDir::new("static/homepage"))
+        .nest_service("/index.html", ServeDir::new("static/homepage"))
+        .nest_service("/page_1", ServeDir::new("static/page_1"))
+        .nest_service("/page_1/index.html", ServeDir::new("static/page_1"));
 
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
