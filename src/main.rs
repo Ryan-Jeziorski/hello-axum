eca1ce03feac35f4435090aeb8ea5fcd3bd7e7f3
Ryan <rjeziorski@outlook.com>
2024-07-25 20:18:19 -0500

    Update routes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 use axum::{
-    // response::Html, 
-    // routing::get, 
+    response::Html, 
+    routing::get, 
     Router
 };
 use tower_http::services::ServeDir;
+
+async fn footer() -> Html<&'static str> {
+    Html(" \
+<footer> \
+<h1 style=\"font-size: small;\">Site Map</h1> \
+    <a href=\"./index.html\">Home</a> \
+    <a href=\"../page_1/index.html\">Test</a> \
+</footer> \
+    ")
+}
 
 #[tokio::main]
 async fn main() {
@@ -12,11 +22,12 @@ async fn main() {
     // TODO: Note that I'm probably doing this the wrong way at the moment
     //       but I'm still getting used to Rust and Axum
     let app = Router::new()
-        .route_service("/", ServeDir::new("static/homepage"))
-        .nest_service("/homepage", ServeDir::new("static/homepage"))
-        .nest_service("/index.html", ServeDir::new("static/homepage"))
-        .nest_service("/page_1", ServeDir::new("static/page_1"))
-        .nest_service("/page_1/index.html", ServeDir::new("static/page_1"));
+        .route_service("/", ServeDir::new("site/homepage"))
+        .nest_service("/homepage", ServeDir::new("site/homepage"))
+        .nest_service("/index.html", ServeDir::new("site/homepage"))
+        .nest_service("/page_1", ServeDir::new("site/page_1"))
+        .nest_service("/page_1/index.html", ServeDir::new("site/page_1"))
+        .route("/footer", get(footer));
 
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
